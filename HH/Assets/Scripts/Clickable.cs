02e0ed33b2cdee4111d46c6487c1f09d37ecc4b3
Roumiana Bourkova <48831445+rumywanderung@users.noreply.github.com>
2020-02-11 17:00:12 +0100

    working on jump/touch

---
+++ HH/Assets/Scripts/Clickable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour
+{
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "tile")
+        {
+            collision.gameObject.tag = "clickable";
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "tile")
+        {
+            collision.gameObject.tag = "clickable";
+        }
+    }
+
+    //private void OnTriggerExit(Collider collision)
+    //{
+    //    if (collision.gameObject.tag == "clickable")
+    //    {
+    //        collision.gameObject.tag = "tile";
+    //    }
+    //}
+    
+}
---
+++ HH/Assets/Scripts/Jump.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Ray ray;
+    bool moving;
+    Vector3 dir;
+   
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        
+        moving = false;
+        dir = new Vector3(-1.51F, -3.22F, 0);
+    }
+
+ 
+
+    void Update()
+    {
+        Debug.Log(moving);
+
+        if (moving == true)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, dir, 0.9F);
+            moving = false;
+        }
+        
+
+        #region mobile
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity) && hit.collider.gameObject.tag == "clickable")
+            {
+                
+                moving = true;
+                dir = hit.collider.gameObject.transform.position;
+                dir.y += 1;
+                
+            }
+        }
+
+        #endregion
+
+        #region pc
+
+        //Debug.Log(this.transform.position.x + "," + this.transform.position.y + "," + this.transform.position.z);
+
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+      
+
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+            {
+                
+
+                if (hit.collider.gameObject.tag == "clickable")
+                {
+                   
+                    moving = true;
+                    dir = hit.collider.gameObject.transform.position;
+                    dir.y += 1;
+                    
+                }
+            } 
+        }
+        #endregion
+    }
+}
